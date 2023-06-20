4b9979ae692f3526727c9516dc8fd8bd37885841
denbarabraza <denis.bareischev@gmail.com>
2023-06-20 12:15:27 +0300

    chore: install and customization eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,3 @@
-export const App = () => <h1>Hello from React!</h1>;
+export const App = () => {
+  return <h1>Hello from React!</h1>;
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.tsx",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "build"),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx|ts|tsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader', 'ts-loader'],
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "public", "index.html"),
-        }),
+  entry: './src/index.tsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'ts-loader'],
+      },
     ],
-    devServer: {
-        static: {
-            directory: path.join(__dirname, "build"),
-        },
-        port: 3000,
-    }
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'build'),
+    },
+    port: 3000,
+  },
 };
