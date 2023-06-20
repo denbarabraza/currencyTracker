0b666684c2de3dfa119e4c8c4b2a4fe6571ea9d3
denbarabraza <denis.bareischev@gmail.com>
2023-06-20 12:39:51 +0300

    chore: configuring paths(alias) in tsconfig and webpack

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { App } from './App';
+import { App } from '@/App';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(<App />);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -17,6 +18,14 @@ module.exports = {
     ],
   },
   resolve: {
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: path.join(__dirname, './tsconfig.json'),
+      }),
+    ],
+    alias: {
+      '@/*': path.resolve(__dirname, 'src'),
+    },
     extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
   },
   plugins: [
