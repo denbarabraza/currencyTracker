ae070d3f36c0dba2d3224f0d40a5d10402f91620
denbarabraza <denis.bareischev@gmail.com>
2023-06-30 12:27:50 +0300

    fix: portal for a modal window

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createPortal } from 'react-dom';
 
 import { Currency } from '@/components/Currency';
 import { currencyQuotes } from '@/constants/currency';
@@ -9,11 +10,12 @@ import { ModalContent, ModalItem } from './styled';
 export const CurrencyModal: React.FC<IModalProps> = ({ currency, isOpen, onClose }) => {
   const currentCurrency = currencyQuotes.find(cur => cur.name === currency);
 
-  return (
+  return createPortal(
     <ModalItem isOpen={isOpen} onClick={onClose}>
       <ModalContent isOpen={isOpen} onClick={e => e.stopPropagation()}>
         {currentCurrency ? <Currency currentCurrency={currentCurrency} /> : null}
       </ModalContent>
-    </ModalItem>
+    </ModalItem>,
+    document.body,
   );
 };
